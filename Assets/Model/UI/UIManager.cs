1764a97f8f50cf17baad9b878f79f137f86309df
daniil.melnikau <daniil_melnikau1@epam.com>
2023-01-11 18:47:58 +0300

    Guide added

--- Assets/Model/UI/UIManager.cs
+++ Assets/Model/UI/UIManager.cs
@@ -3,12 +3,36 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
+
+    public bool showPlayerMoves;
     public Canvas finishCanvas;
     public Canvas deathCanvas;
+    public Animator handAnimator;
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        handAnimator.gameObject.SetActive(showPlayerMoves);
+    }
+
+    private void Update()
+    {
+        if (showPlayerMoves && Time.timeScale > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Space))
+            {
+                handAnimator.SetTrigger("tap");
+            }
+
+            if (Input.GetKeyUp(KeyCode.Mouse0) || Input.GetKeyUp(KeyCode.Space))
+            {
+                handAnimator.SetTrigger("release");
+            }
+        }
     }
 
     public void Restart()
